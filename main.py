83e4f4c4b012f780ef3dcc1c43964411f6b949bc
McKenzie <AkeemMcKenzie@regencycenters.com>
2021-07-22 11:18:29 -0400

    saved

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ layoutprefile = [
     [sg.Text('Select the two files you wish to use')],
     [sg.Text('File 1'), sg.InputText(),sg.FileBrowse()],
     [sg.Text('File 2'), sg.InputText(),sg.FileBrowse()],
-    #---List One
-    [sg.Output(size=(61,5))],
-    [sg.Submit('Proceed'), sg.Cancel('Exit')]
+    #---List One---#
+    #[sg.Output(size=(61,5))],
+    [sg.Submit('Next'), sg.Cancel('Exit')]
 ]
 window1 = sg.Window('University of North Florida CSV Comparison Tool', layoutprefile)
 while True:
@@ -22,7 +22,7 @@ while True:
     if event in (None, 'Exit', 'Cancel'):
         Window2 = 0
         break
-    elif event == 'Proceed':
+    elif event == 'Next':
         #now we check if two same file types have been selected 
         file1temp = file2temp = pass_stage = next_stage = None
         file1, file2 = values[0],values[1]
